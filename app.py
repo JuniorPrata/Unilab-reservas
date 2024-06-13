fd786f1c0abb71d69ccb49c7f36552d666b89ea6
eujuniorprata <eujuniorprata@gmail.com>
2024-06-12 23:23:40 -0300

    add

--- app.py
+++ app.py
@@ -55,12 +55,16 @@ def reserva():
         time = request.form['time']
         reservas.append({"lab": lab, "date": date, "time": time})
         flash('Reserva realizada com sucesso!')
-        return redirect(url_for('pagina_principal'))
+        return redirect(url_for('sucesso'))
     return render_template('reserva.html')
 
 @app.route('/labs')
 def labs():
     return render_template('labs.html')
+
+@app.route('/sucesso')
+def sucesso():
+    return render_template('efetuada.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
