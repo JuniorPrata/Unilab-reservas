cbb3bbbb954f266e81155d80992059c0f3ac7c5b
eujuniorprata <eujuniorprata@gmail.com>
2024-06-13 00:01:58 -0300

    add

--- app.py
+++ app.py
@@ -58,9 +58,9 @@ def reserva():
         return redirect(url_for('sucesso'))
     return render_template('reserva.html')
 
-@app.route('/labs')
-def labs():
-    return render_template('labs.html')
+@app.route('/reserva-semana')
+def semana():
+    return render_template('reserva-semana.html')
 
 @app.route('/sucesso')
 def sucesso():
